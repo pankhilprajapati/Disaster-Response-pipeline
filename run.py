da791090c6bc8474025548c702a9b9177f71e1c7
pankhilprajapati <pankhil.prajapati2017@vitstudent.ac.in>
2020-04-21 21:53:06 +0530

    some changes

--- run.py
+++ run.py
@@ -28,11 +28,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
+engine = create_engine('sqlite:///./data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+model = joblib.load("./models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -215,4 +215,5 @@ def go():
         query=query,
         classification_result=classification_results
     )
+
 
